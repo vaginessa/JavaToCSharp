b1edbda1ab3fc2b1c69f0b3ebc4980b3fd9432ec
Ruben Adriaanse <R.M.Adriaanse@dnb.nl>
2018-07-12 18:22:12 +0200

    Write whole directories.

--- JavaToCSharpCli/Program.cs
+++ JavaToCSharpCli/Program.cs
@@ -1,6 +1,8 @@
 ﻿using JavaToCSharp;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace JavaToCSharpCli
 {
@@ -10,29 +12,55 @@ namespace JavaToCSharpCli
         {
             if (args == null || args.Length < 2)
             {
-                Console.WriteLine("Usage: JavaToCSharpCli.exe [pathToJavaFile] [pathToCsOutputFile]");
+                Console.WriteLine("Usage: JavaToCSharpCli.exe [pathToJavaDir] [pathToCsOutputDir]");
                 return;
             }
             
-            if (!File.Exists(args[0]))
+            //if (!File.Exists(args[0]))
+            //{
+            //    Console.WriteLine("Java input file doesn't exist!");
+            //    return;
+            //}
+
+            var inputDir = args[0];
+            var outputDir = args[1];
+
+            var inputInfo = new DirectoryInfo(inputDir);
+            var outputInfo = new DirectoryInfo(outputDir);
+
+            var files = inputInfo.GetFiles("*.java", SearchOption.AllDirectories);
+            var filePaths = files.Select(x => x.FullName).ToList();
+
+            if (filePaths.Any())
             {
-                Console.WriteLine("Java input file doesn't exist!");
-                return;
+                var options = new JavaConversionOptions();
+                var inOutPairs = filePaths.ToDictionary(fileIn => fileIn, fileIn => $"{fileIn.Replace(inputInfo.FullName, outputInfo.FullName)}.cs");
+                WriteAllFiles(inOutPairs, options);
             }
 
-            var javaText = File.ReadAllText(args[0]);
-
-            // HACK for testing
-            var options = new JavaConversionOptions()
-                .AddPackageReplacement("org\\.apache\\.lucene", "Lucene.Net")
-                .AddUsing("Lucene.Net")
-                .AddUsing("Lucene.Net.Support")
-                .AddUsing("Lucene.Net.Util");
-
-            var parsed = JavaToCSharpConverter.ConvertText(javaText, options);
-
-            File.WriteAllText(args[1], parsed);
             Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+        private static void WriteAllFiles(Dictionary<string, string> files, JavaConversionOptions options)
+        {
+            foreach (var file in files)
+            {
+                var directoryName = new FileInfo(file.Value).DirectoryName;
+                if (directoryName != null) Directory.CreateDirectory(directoryName);
+                var javaText = File.ReadAllText(file.Key);
+
+                try
+                {
+                    var parsed = JavaToCSharpConverter.ConvertText(javaText, options);
+                    File.WriteAllText(file.Value, parsed);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine($"Skipping file {file.Value} that would write to {file.Key}\n\n");
+                }
+            }
         }
     }
 }
