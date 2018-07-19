405b7550a3d51b3bf19ded66631b36d362a53333
Nicolas Deslandes <ndeslandes@live.fr>
2018-07-19 15:23:43 +0200

    Added unit tests.

---
+++ JavaToCSharp.Tests/MethodDeclarationTests.cs
@@ -0,0 +1,44 @@
+﻿using com.github.javaparser;
+using com.github.javaparser.ast.body;
+using JavaToCSharp.Declarations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace JavaToCSharp.Tests
+{
+    public class MethodDeclarationTests
+    {
+        [Fact]
+        public void TestSimpleMethodDeclaration()
+        {
+            var java = @"public static void write() {}";
+            var csharp = @"public static void Write()
+{
+}";
+
+            var converted = ConvertStatement(java);
+            Assert.Equal(csharp, converted);
+        }
+
+        private static string ConvertStatement(string java)
+        {
+            var javaClassDeclaration = @"
+            class A
+            {
+                " + java + @"
+            }";
+            var declaration = JavaParser.parseBodyDeclaration(javaClassDeclaration);
+            var options = new JavaConversionOptions();
+            var context = new ConversionContext(options);
+            var classDeclaration = SyntaxFactory.ClassDeclaration("A");
+            var body = declaration.getChildrenNodes().get(1);
+            var statementSyntax = BodyDeclarationVisitor.VisitBodyDeclarationForClass(context,
+                classDeclaration, (BodyDeclaration)body)
+                .NormalizeWhitespace();
+
+            var tree = CSharpSyntaxTree.Create(statementSyntax);
+            return tree.GetText().ToString();
+        }
+    }
+}
---
+++ JavaToCSharp.Tests/TypeExprTests.cs
@@ -0,0 +1,31 @@
+﻿using com.github.javaparser;
+using JavaToCSharp.Statements;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace JavaToCSharp.Tests
+{
+    public class TypeExprTests
+    {
+        [Fact]
+        public void TestTypeReferenceStatement()
+        {
+            var java = @"myList.forEach(writer::write);";
+            var csharp = @"myList.ForEach(writer.write);";
+
+            var converted = ConvertStatement(java);
+            Assert.Equal(csharp, converted);
+        }
+
+        private static string ConvertStatement(string java)
+        {
+            var statement = JavaParser.parseStatement(java);
+            var options = new JavaConversionOptions();
+            var context = new ConversionContext(options);
+            var statementSyntax = StatementVisitor.VisitStatement(context, statement);
+
+            var tree = CSharpSyntaxTree.Create(statementSyntax);
+            return tree.GetText().ToString();
+        }
+    }
+}
